22c4c18d1629cba0dd5a4a6c6e0285714b0956fc
Wangui Githua <barb.wangui@gmail.com>
2025-06-14 20:56:20 -0400

    Initalize and load tasks from local storage

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ document.addEventListener('DOMContentLoaded', function () {
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById ("task-input");
     const taskList = document.getElementById("task-list");
+    let tasks = []
 
+    //Load Tasks
+    function loadTasksFromStorage() {
+        const savedTasks = localStorage.getItem('tasks');
+        
+        // check for tasks
+        if (savedTasks){
+            const tasks = JSON.parse(savedTasks);
+
+            tasksArray.forEach(task => createTaskElement(task.text));
+
+        }
+    }
+
+
+    
+    
 
     function addTask(){
         // get trimmed value
