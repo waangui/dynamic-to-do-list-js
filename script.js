526660f6845fc49020fe2aa5992ff0e6cdf5ba58
Wangui Githua <barb.wangui@gmail.com>
2025-06-14 20:22:43 -0400

    set up event listener for page, created DOM elements and set up add task

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+
+// DOM Elements
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById ("task-input");
+    const taskList = document.getElementById("task-list");
+
+
+    function addTask(){
+        // get trimmed value
+
+        const taskTest = taskInput.value.trim();
+
+        // check if empty
+
+        if (!taskTest){
+            alert("Please enter a task!");
+        }
+    }
+
+})
